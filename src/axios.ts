3c66644554ece3809d95e9057e4a7baf3ae91c40
deepakwebsenor05-a11y <deepak.websenor05@gmail.com>
2025-10-25 10:09:08 +0530

    update axios 2

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://voice-frontend.netlify.app/api',
+    baseURL: 'https://voice-saas-1.onrender.com/api',
     withCredentials: true,
 });
 
